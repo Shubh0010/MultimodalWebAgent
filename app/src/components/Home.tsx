4901a8cd3fc89e1e23b24d2e1864fc9cb21cd729
shubh0010 <shubhamnegi0010@gmail.com>
2024-05-12 02:13:11 +0530

    iframe UI updates

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -1,8 +1,13 @@
 function HomeComponent() {
   return (
-
-    <div className='w-full h-full'>
-      <iframe src="https://multimodal-web-agent-gamma.vercel.app/chat" title="Embedded Content" className='w-full h-full border-none'></iframe>
+    <div>
+      <iframe
+        src="https://multimodal-web-agent-gamma.vercel.app/chat"
+        title="Embedded Content"
+        className="absolute w-full h-full"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
