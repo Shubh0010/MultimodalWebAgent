34cf839654a30e05864e401ca86222231bc6977a
shubh0010 <shubhamnegi0010@gmail.com>
2024-05-12 02:00:52 +0530

    iframe introduced

---
+++ app/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomeComponent() {
+  return (
+
+    <div className='w-full h-full'>
+      <iframe src="https://multimodal-web-agent-gamma.vercel.app/chat" title="Embedded Content" className='w-full h-full border-none'></iframe>
+    </div>
+  );
+}
+
+export default HomeComponent;
