87ed11712a332b1f476f7f0ee5af166341ec11b7
shubh0010 <shubhamnegi0010@gmail.com>
2024-05-12 11:11:11 +0530

    ui fixes

--- app/src/components/QueryBox.tsx
+++ app/src/components/QueryBox.tsx
@@ -13,14 +13,14 @@ export const QueryBox = () => {
   return <div className="w-full md:w-1/3 bg-white p-6 space-y-4 flex flex-col justify-between">
 
     <div className="space-y-4">
-      <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg">
+      <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg hover:cursor-pointer hover:bg-slate-200">
         <CloudIcon className="h-6 w-6 text-gray-600" />
         <div className="flex flex-col">
           <span className="font-medium">Add the book Zero to One</span>
           <span className="text-sm text-gray-500">in my Amazon cart</span>
         </div>
       </div>
-      <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg">
+      <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg hover:cursor-pointer hover:bg-slate-200">
         <GithubIcon className="h-6 w-6 text-gray-600" />
         <div className="flex flex-col">
           <span className="font-medium">What's the top post</span>
@@ -28,14 +28,14 @@ export const QueryBox = () => {
         </div>
 
       </div>
-      <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg">
+      <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg hover:cursor-pointer hover:bg-slate-200">
         <MenuIcon className="h-6 w-6 text-gray-600" />
         <div className="flex flex-col">
           <span className="font-medium">Reserve a table at Fogo de Chão</span>
           <span className="text-sm text-gray-500">in San Jose for 2 people via OpenTable</span>
         </div>
       </div>
-      <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg">
+      <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg hover:cursor-pointer hover:bg-slate-200">
         <CandlestickChartIcon className="h-6 w-6 text-gray-600" />
         <div className="flex flex-col">
           <span className="font-medium">How much did NVIDIA stock</span>
@@ -48,8 +48,8 @@ export const QueryBox = () => {
       <div className="mt-4" />
       <div className="flex items-center space-x-4 rounded-lg border border-gray-300 px-4 py-2">
         <input className="flex-1" placeholder="Write a command..." />
-        <MicIcon className="h-6 w-6 text-gray-600" />
-        <InfoIcon className="h-6 w-6 text-gray-600" />
+        <MicIcon className="h-6 w-6 text-gray-600 hover:cursor-pointer" />
+        <InfoIcon className="h-6 w-6 text-gray-600 hover:cursor-pointer" />
       </div>
       <div className="flex justify-between mx-48">
         <button>
